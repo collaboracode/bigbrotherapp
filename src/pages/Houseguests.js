abc61ca0271c1c124e8fbea79412be568656cfbd
Michael Kielpinski <mtsk321@gmail.com>
2022-07-12 23:18:22 -0700

    added start to ReturnGuestOrGuests input options, and fixed a bug that caused 0 to be falsy

--- src/pages/Houseguests.js
+++ src/pages/Houseguests.js
@@ -11,21 +11,22 @@ import ReturnGuests from "../utility/ReturnGuestOrGuests"
 
 const Houseguests = () => {
     const navigate = useNavigate()
-    const height = '100'
+    // const height = '100'
     const width = '100%'
     const handleImageClick = (imageUrl, name) => {
         navigate('/profile', { state: { imageUrl, name } })
 
     }
 
-    const arr = ReturnGuests({end: 4 })
-    const [guests, setGuests] = useState(arr)
+    const arr = ReturnGuests({start: 0, end: 1})
+    const [guests, setGuests] = useState()
+    !guests && setGuests(arr)
     const showGuests = () => {
         return (
             guests && guests.map((guest, i) => {
                 return (
                     <Col sm="3" key={i}>
-                        <img key={i} onClick={() => {
+                        <img alt="" key={i} onClick={() => {
                             handleImageClick(guest.img, guest.name)
                         }}
                             width={width}
@@ -33,10 +34,9 @@ const Houseguests = () => {
                     </Col>
                 )
             })
-
-
         )
     }
+
     return (
         <Container>
             <h1 className="text-primary">Houseguests</h1>
@@ -55,5 +55,4 @@ const Houseguests = () => {
         </Container>
     )
 }
-
 export default Houseguests
--- src/utility/ReturnGuestOrGuests.js
+++ src/utility/ReturnGuestOrGuests.js
@@ -2,21 +2,21 @@ const jsonData = require("./guests.json")
 
 /**
  * this is a temp way of getting this, will change with api,
- * but this should abstract away the implementation of it
+ * but this should abstract away the implementation of it,
+ * so the format of the return should probably stay the same.
  * 
- * @param {Object} obj
+ * @param {Object} obj **optional**
  * @param {number | undefined} obj.id **optional**
  * @param {number | undefined} obj.end **optional**
- * 
  */
 export default function getGuests(obj = { id: undefined, start: undefined, end: undefined }) {
   if (obj.id && jsonData) {
     const filteredData = jsonData.filter(guest => guest.id === obj.id)
-    console.log(filteredData)
     return filteredData
   } else if (!obj.id && jsonData) {
-    if (obj.end) {
-      return jsonData.slice(obj.start ? obj.start : 0, Number(obj.end))
+    if (obj.start !== undefined | obj.end !== undefined) {
+      return jsonData.slice(obj.start !== undefined ? Number(obj.start)
+      : 0, obj.end !== undefined ? Number(obj.end + 1) : jsonData.length)
     } else {
       return jsonData
     }
